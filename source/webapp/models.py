3c13499b21420975b62810f3eee6820e5a1cd3c9
Emil <emilrakaev@gmail.com>
2020-07-24 00:00:19 +0600

    Add page with detail view of article

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
 
+STATUS_CHOICES = [
+    ('new', 'Не модерированно'),
+    ('moderated', 'Модерированно'),
+    ('rejected', 'Отклоненно')
+]
+
+
+
 class Article(models.Model):
     title = models.CharField(max_length=200, null=False, blank=False, verbose_name='Заголовок')
     text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Текст')
     author = models.CharField(max_length=40, null=False, blank=False, default='Unknown', verbose_name='Автор')
+    status = models.CharField(max_length=15, choices = STATUS_CHOICES,default='new', verbose_name='Модерация')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
 
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,25 +1,46 @@
 from django.shortcuts import render
-from webapp.models import Article
+from webapp.models import Article, STATUS_CHOICES
 from django.http import HttpResponseNotAllowed
 
 
 def index_view(request):
     data = Article.objects.all()
-
     return render(request, 'index.html', context={
         'articles': data
     })
 
 
+def article_view(request):
+    article_id = request.GET.get('pk')
+    article = Article.objects.get(pk=article_id)
+    context = {'article': article}
+    return render(request, 'article_view.html', context)
+
+
 def article_create_view(request):
     if request.method == 'GET':
-        return render(request, 'article_create.html')
+        return render(request, 'article_create.html', context={'status_choices': STATUS_CHOICES
+                                                               })
     elif request.method == 'POST':
         title = request.POST.get('title')
         text = request.POST.get('text')
         author = request.POST.get('author')
-        article = Article.objects.create(title = title, text = text, author = author)
+        status = request.POST.get('status')
+        article = Article.objects.create(title=title, text=text, author=author, status=status)
         context = {'article': article}
         return render(request, 'article_view.html', context)
     else:
         return HttpResponseNotAllowed(permitted_methods=['GET', 'POST'])
+
+
+def delete_article(request):
+    if request.method == 'GET':
+        return render(request, 'delete_form.html')
+    elif request.method == 'POST':
+        id_article = request.POST.get('id')
+        article = Article.objects.get(pk=id_article)
+        article.delete()
+        data = Article.objects.all()
+        return render(request, 'index.html', context={
+            'articles': data
+        })
