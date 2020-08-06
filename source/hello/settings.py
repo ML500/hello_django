cb6da2ef12019f83b1b99acaf5d0bda97499a639
Emil <emilrakaev@gmail.com>
2020-08-06 19:45:20 +0600

    Добавлена модель тега и связь Many to many между ним и статьей

--- source/hello/settings.py
+++ source/hello/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*+jy#09u%onug2e!r+cx1!ucny%hb)%z88#+sp0ik&th*31+y#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
 
 
 # Application definition
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'webapp'
+    'widget_tweaks',
+    'webapp',
 ]
 
 MIDDLEWARE = [
--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import STATUS_CHOICES
+from .models import STATUS_CHOICES, Article
 
 default_status = STATUS_CHOICES[0][0]
 
@@ -10,8 +10,10 @@ class ArticleForm(forms.Form):
     author = forms.CharField(max_length=40, required=True, initial='Unknown', label='Автор')
     status = forms.ChoiceField(choices=STATUS_CHOICES, initial=default_status, label='Модерация')
     publish_at = forms.DateTimeField(required=False, label='Время публикации',
-                                    input_formats=['%Y-%m-%d', '%Y-%m-%dT%H:%M', '%Y-%m-%dT%H:%M:%S', '%Y-%m-%d %H:%M',
-                                                   '%Y-%m-%d %H:%M:%S'],
-                                    widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}))
-# для полей типа DateField
-# publish_at = forms.DateField(..., widget=forms.DateInput(attrs={'type': 'date'}))
+                                     input_formats=['%Y-%m-%d', '%Y-%m-%dT%H:%M', '%Y-%m-%dT%H:%M:%S', '%Y-%m-%d %H:%M',
+                                                    '%Y-%m-%d %H:%M:%S'],
+                                     widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}))
+
+
+class CommentForm(forms.Form):
+    article = forms.ModelChoiceField(queryset=Article.objects.all(), required=True, label='Article')
